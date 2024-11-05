d7bd57fcf39d07fa1b0eafb9d28db2d6c9afc2e4
aayushtailang1911 <aayush.tailang@nitorinfotech.com>
2024-11-05 12:05:04 +0530

    folder structure is ready

---
+++ demo/src/main/java/com/CibilCalculator/CibilCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.CibilCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibilCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibilCalculatorApplication.class, args);
+	}
+
+}
