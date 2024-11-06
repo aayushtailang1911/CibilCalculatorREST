97454c5b0b396c0de26bef57e6ce82d9cb81ae19
aayushtailang1911 <aayush.tailang@nitorinfotech.com>
2024-11-06 12:39:07 +0530

    Made changes in the attribute entity and added spark and hadoop dependencies. (#3)
    * attribute entity added
    
    * attribute as a plain entity added and added spark and hadoop dependencies

---
+++ demo/src/main/java/com/CibilCalculator/entities/Attribute.java
@@ -0,0 +1,27 @@
+package com.CibilCalculator.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+/**
+ * POJO representation for Attributes in Model Registry json file
+ */
+public class Attribute implements Serializable{
+
+    private String name;
+
+    private Boolean required;
+
+    private Boolean nullable;
+
+}
