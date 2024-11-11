74d1e16935adf266b0e58fb60384ec89bc21320f
PruthvirajNitor <pruthviraj.jadhav@nitorinfotech.com>
2024-11-11 15:47:20 +0530

    Feature/adding model data/pj (#12)
    * added model data
    
    * rename model data
    
    * deleted .idea

---
+++ demo/src/main/java/com/CibilCalculator/modeldata/RecentInquiry.java
@@ -0,0 +1,43 @@
+package com.CibilCalculator.modeldata;
+
+public class RecentInquiry implements IAttribute{
+
+    private Double recentInquiryScore;
+
+    public RecentInquiry(){
+        this.recentInquiryScore = 0.0;
+    };
+
+    public RecentInquiry(int recentInquiryIndex )
+    {
+        this.recentInquiryScore = this.calculateScore(recentInquiryIndex);
+    }
+
+
+    public Double calculateScore( int recentInquiryIndex) {
+        //recentInquiryIndex = Credit Score Inquiry raised in last x months;
+        return switch (recentInquiryIndex){
+            case 0 -> 100.00;
+            case  1-> 80.00;
+            case 2 -> 60.00;
+            case 3 -> 40.00;
+            case 4 -> 30.00;
+            default -> 0.00;
+        };
+    }
+
+    @Override
+    public Double getScore() {
+        return this.recentInquiryScore;
+    }
+
+    @Override
+    public String getName() {
+        return "RecentInquiry";
+    }
+
+    @Override
+    public String toString() {
+        return "RecentInquiryScore= " + recentInquiryScore;
+    }
+}
